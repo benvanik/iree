6286ae1d770cdd7c4c2b556eb9becded9d70e81f
Ben Vanik <ben.vanik@gmail.com>
2024-10-03 15:38:14 -0700

    [WIP] AMDGPU

---
+++ runtime/src/iree/hal/drivers/amdgpu/device/blit.h
@@ -0,0 +1,81 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_HAL_DRIVERS_AMDGPU_DEVICE_BLIT_H_
+#define IREE_HAL_DRIVERS_AMDGPU_DEVICE_BLIT_H_
+
+#include "iree/hal/drivers/amdgpu/device/kernels.h"
+#include "iree/hal/drivers/amdgpu/device/support/common.h"
+#include "iree/hal/drivers/amdgpu/device/support/queue.h"
+#include "iree/hal/drivers/amdgpu/device/tracing.h"
+
+//===----------------------------------------------------------------------===//
+// Blit Kernels
+//===----------------------------------------------------------------------===//
+
+#define IREE_HAL_AMDGPU_DEVICE_BUFFER_FILL_KERNARG_SIZE (3 * sizeof(void*))
+#define IREE_HAL_AMDGPU_DEVICE_BUFFER_FILL_KERNARG_ALIGNMENT 8
+#define IREE_HAL_AMDGPU_DEVICE_BUFFER_COPY_KERNARG_SIZE (3 * sizeof(void*))
+#define IREE_HAL_AMDGPU_DEVICE_BUFFER_COPY_KERNARG_ALIGNMENT 8
+
+// State used when scheduling transfer commands.
+typedef struct iree_hal_amdgpu_device_buffer_transfer_state_t {
+  // Target queue that will execute the transfer operation.
+  iree_hsa_queue_t* queue;
+  // Handles to opaque kernel objects used to dispatch builtin kernels.
+  const iree_hal_amdgpu_device_kernels_t* kernels;
+  // Optional trace buffer used when tracing infrastructure is available.
+  iree_hal_amdgpu_device_trace_buffer_t* trace_buffer;
+} iree_hal_amdgpu_device_buffer_transfer_state_t;
+
+#if defined(IREE_AMDGPU_TARGET_DEVICE)
+
+// Emplaces a fill dispatch packet in the target queue at the given index.
+// The queue doorbell will not be signaled.
+//
+// NOTE: this only works with blits today. SDMA will require a different
+// signature.
+iree_hsa_kernel_dispatch_packet_t* IREE_AMDGPU_RESTRICT
+iree_hal_amdgpu_device_buffer_fill_emplace_reserve(
+    const iree_hal_amdgpu_device_buffer_transfer_state_t* IREE_AMDGPU_RESTRICT
+        state,
+    void* target_ptr, const uint64_t length, const uint64_t pattern,
+    const uint8_t pattern_length, uint64_t* IREE_AMDGPU_RESTRICT kernarg_ptr,
+    const uint64_t packet_id);
+
+// Enqueues a fill dispatch packet in the target queue.
+// The packet will be acquired at the current write_index and the queue doorbell
+// will be signaled.
+void iree_hal_amdgpu_device_buffer_fill_enqueue(
+    const iree_hal_amdgpu_device_buffer_transfer_state_t* IREE_AMDGPU_RESTRICT
+        state,
+    void* target_ptr, const uint64_t length, const uint64_t pattern,
+    const uint8_t pattern_length, uint64_t* IREE_AMDGPU_RESTRICT kernarg_ptr);
+
+// Emplaces a copy dispatch packet in the target queue at the given index.
+// The queue doorbell will not be signaled.
+//
+// NOTE: this only works with blits today. SDMA will require a different
+// signature.
+iree_hsa_kernel_dispatch_packet_t* IREE_AMDGPU_RESTRICT
+iree_hal_amdgpu_device_buffer_copy_emplace_reserve(
+    const iree_hal_amdgpu_device_buffer_transfer_state_t* IREE_AMDGPU_RESTRICT
+        state,
+    const void* source_ptr, void* target_ptr, const uint64_t length,
+    uint64_t* IREE_AMDGPU_RESTRICT kernarg_ptr, const uint64_t packet_id);
+
+// Enqueues a copy dispatch packet in the target queue.
+// The packet will be acquired at the current write_index and the queue doorbell
+// will be signaled.
+void iree_hal_amdgpu_device_buffer_copy_enqueue(
+    const iree_hal_amdgpu_device_buffer_transfer_state_t* IREE_AMDGPU_RESTRICT
+        state,
+    const void* source_ptr, void* target_ptr, const uint64_t length,
+    uint64_t* IREE_AMDGPU_RESTRICT kernarg_ptr);
+
+#endif  // IREE_AMDGPU_TARGET_DEVICE
+
+#endif  // IREE_HAL_DRIVERS_AMDGPU_DEVICE_BLIT_H_
---
+++ runtime/src/iree/hal/drivers/amdgpu/device/buffer.c
@@ -0,0 +1,67 @@
+// Copyright 2024 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "iree/hal/drivers/amdgpu/device/buffer.h"
+
+//===----------------------------------------------------------------------===//
+// iree_hal_amdgpu_device_buffer_ref_t
+//===----------------------------------------------------------------------===//
+
+// TODO(benvanik): simplify this for command buffers by pre-baking as much as we
+// can during the queue issue - we can at least dereference handles and add in
+// the offset for everything such that we only have to deal with the slot offset
+// and have less branchy code.
+void* iree_hal_amdgpu_device_buffer_ref_resolve(
+    iree_hal_amdgpu_device_buffer_ref_t buffer_ref,
+    IREE_AMDGPU_ALIGNAS(64)
+        const iree_hal_amdgpu_device_buffer_ref_t* IREE_AMDGPU_RESTRICT
+            binding_table) {
+  if (buffer_ref.type == IREE_HAL_AMDGPU_DEVICE_BUFFER_TYPE_SLOT) {
+    const iree_hal_amdgpu_device_buffer_ref_t binding =
+        binding_table[buffer_ref.value.slot];
+    // DO NOT SUBMIT may not be right
+    const uint64_t offset = buffer_ref.offset + binding.offset;
+    const uint64_t length = buffer_ref.length == UINT64_MAX
+                                ? binding.length - buffer_ref.offset
+                                : buffer_ref.length;
+    buffer_ref = (iree_hal_amdgpu_device_buffer_ref_t){
+        .type = binding.type,
+        .offset = offset,
+        .length = length,
+        .value.bits = binding.value.bits,
+    };
+  }
+  if (buffer_ref.type == IREE_HAL_AMDGPU_DEVICE_BUFFER_TYPE_HANDLE) {
+    buffer_ref.value.ptr = buffer_ref.value.handle->ptr;
+  }
+  return buffer_ref.value.ptr
+             ? (uint8_t*)buffer_ref.value.ptr + buffer_ref.offset
+             : NULL;
+}
+
+void* iree_hal_amdgpu_device_workgroup_count_buffer_ref_resolve(
+    iree_hal_amdgpu_device_workgroup_count_buffer_ref_t buffer_ref,
+    IREE_AMDGPU_ALIGNAS(64)
+        const iree_hal_amdgpu_device_buffer_ref_t* IREE_AMDGPU_RESTRICT
+            binding_table) {
+  if (buffer_ref.type == IREE_HAL_AMDGPU_DEVICE_BUFFER_TYPE_SLOT) {
+    const iree_hal_amdgpu_device_buffer_ref_t binding =
+        binding_table[buffer_ref.value.slot];
+    // DO NOT SUBMIT may not be right
+    const uint64_t offset = buffer_ref.offset + binding.offset;
+    buffer_ref = (iree_hal_amdgpu_device_workgroup_count_buffer_ref_t){
+        .type = binding.type,
+        .offset = offset,
+        .value.bits = binding.value.bits,
+    };
+  }
+  if (buffer_ref.type == IREE_HAL_AMDGPU_DEVICE_BUFFER_TYPE_HANDLE) {
+    buffer_ref.value.ptr = buffer_ref.value.handle->ptr;
+  }
+  return buffer_ref.value.ptr
+             ? (uint8_t*)buffer_ref.value.ptr + buffer_ref.offset
+             : NULL;
+}
