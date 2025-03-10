0649169c639fc643790712ce62df03069f0b00e2
Ben Vanik <ben.vanik@gmail.com>
2025-03-10 08:41:14 -0700

    WIP passes

--- compiler/src/iree/compiler/Dialect/Stream/Transforms/Passes.cpp
+++ compiler/src/iree/compiler/Dialect/Stream/Transforms/Passes.cpp
@@ -243,6 +243,10 @@ void buildStreamAsyncPassPipeline(OpPassManager &passManager,
 
 void buildStreamCmdPassPipeline(OpPassManager &passManager,
                                 const TransformOptions &transformOptions) {
+  // Insert `stream.async.retain`/`stream.async.release` ops to explicitly
+  // define asynchronous resource lifetime.
+  passManager.addPass(IREE::Stream::createAutomaticReferenceCountingPass());
+
   // Schedule fine-grained allocations and insert placeholders for larger/longer
   // lifetime allocations.
   passManager.addPass(IREE::Stream::createScheduleAllocationPass());
@@ -267,6 +271,10 @@ void buildStreamCmdPassPipeline(OpPassManager &passManager,
   // cleanup patterns.
   passManager.addPass(IREE::Util::createPropagateSubrangesPass());
   buildStreamCleanupPassPipeline(passManager, transformOptions);
+
+  // Perform whole-program analysis to try to eliminate as many
+  // stream.async.retain/release ops as is safe.
+  passManager.addPass(IREE::Stream::createElideReferenceCountingPass());
 
   // Convert stream.async.retain/release ops into lower-level resource
   // primitives. Once we reify we lose the information required for analysis and
